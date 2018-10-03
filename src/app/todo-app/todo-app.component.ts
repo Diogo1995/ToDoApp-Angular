4c3911529b40e5cdd51c703f9c97ea32d0d301ff
Diogo Pedrosa <diogo.pedrosa@innowave.tech>
2018-10-03 12:07:24 +0100

    step 8.2

--- src/app/todo-app/todo-app.component.ts
+++ src/app/todo-app/todo-app.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Store, select } from '@ngrx/store';                                
+import { Observable } from 'rxjs';                                          
+
+import { AddTodo, ToggleTodo } from '@state/todo-list/todo-list.actions';   
+import { State } from '@state/todo-list/todo-list.state';
+
+
 @Component({
   selector: 'app-todo-app',
   templateUrl: './todo-app.component.html',
@@ -7,9 +14,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoAppComponent implements OnInit {
 
-  constructor() { }
+  newTodo: String = '';
+  todoList$: Observable<any>;
+
+  constructor(private store: Store<State>) {
+    this.todoList$ = store.pipe(select('todoList'));                        
+  }
 
   ngOnInit() {
   }
 
+  addTodo() {
+    this.store.dispatch(                                                    
+      new AddTodo({ name: this.newTodo })                                 
+    );                                                                      
+  }
+  
+  toggleTodo(index: Number) {                                                 
+    this.store.dispatch(                                                    
+      new ToggleTodo({ index })                                           
+    );                                                                      
+  }
 }
--- src/state/state.module.ts
+++ src/state/state.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 
 import { counterReducer } from '@state/counter/counter.reducer';
+import { todoListReducer } from '@state/todo-list/todo-list.reducer';
 
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
     imports: [
         StoreModule.forRoot({
-            count: counterReducer
+            count: counterReducer,
+            todoList: todoListReducer
         }),
         StoreDevtoolsModule.instrument()
     ]
